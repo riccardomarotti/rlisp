61bbcf04eec5b9dc9b147976aa9dd64a5c23fd0a
Riccardo <riccardo.marotti@gmail.com>
2015-08-22 11:35:22 +0200

    added evaluation

--- parsing.c
+++ parsing.c
@@ -5,6 +5,38 @@
 
 #include "mpc/mpc.h"
 
+
+long eval_operation(long x, char* op, long y) {
+	if (strcmp(op, "+") == 0)
+		return x + y;
+	if (strcmp(op, "-") == 0)
+		return x - y;
+	if (strcmp(op, "*") == 0)
+		return x * y;
+	if (strcmp(op, "/") == 0)
+		return x / y;
+
+	return 0;
+}
+
+long eval(mpc_ast_t* t) {
+	if (strstr(t->tag, "number")) {
+		return atoi(t->contents);
+	}
+
+	char* op = t->children[1]->contents;
+
+	long x = eval(t->children[2]);
+
+	int i = 3;
+	while (strstr(t->children[i]->tag, "expr")) {
+		x = eval_operation(x, op, eval(t->children[i]));
+		i++;
+	}
+
+	return x;
+}
+
 int main(int argc, char** argv)
 {
 	mpc_parser_t* Number   = mpc_new("number");
@@ -31,7 +63,8 @@ int main(int argc, char** argv)
 		mpc_result_t r;
 
 		if (mpc_parse("<stdin>", input, Rlisp, &r)) {
-			mpc_ast_print(r.output);
+			long result = eval(r.output);
+			printf("%li\n", result);
 			mpc_ast_delete(r.output);
 		} else {
 			mpc_err_print(r.error);
